7979095433d98caaf5be2b4bdcd0fd8e49f0aef1
Ritik Ambadi <ambadiritik@gmail.com>
2021-01-11 13:28:01 +0530

    Added Menu-List

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuthState, useAuthDispatch } from '../context/auth';
-import Logo from './Logo';
+import classNames from 'classnames';
 
 const Navbar: React.FC = () => {
+	const [toggle, setToggle] = useState(true);
 	const dispatch = useAuthDispatch();
 	const { authenticated, user } = useAuthState();
 
+	const toggler = e => {
+		setToggle(!toggle);
+		console.log(toggle);
+	};
+
 	return (
-		<nav className='flex flex-col px-4 pt-2 md:px-20'>
+		<nav className='flex flex-col pt-2 border'>
 			{/* Navbar Main */}
-			<div className='flex flex-row items-center justify-between h-16'>
+			<div className='flex flex-row items-center justify-between h-16 px-4 md:px-20'>
 				<div className='flex flex-row items-center space-x-4'>
 					<h1 className='text-sm font-bold tracking-widest text-gray-600 uppercase'>
 						Biblio
@@ -27,7 +33,7 @@ const Navbar: React.FC = () => {
 				<div className='flex flex-row items-center space-x-4'>
 					<Link href='/login'>
 						<a className='mt-2 text-sm font-semibold text-gray-400 transition duration-200 hover:text-gray-600'>
-							<i className='fas fa-user-alt'></i>
+							<i className='fas fa-layer-group'></i>
 						</a>
 					</Link>
 					<Link href='/cart'>
@@ -35,12 +41,76 @@ const Navbar: React.FC = () => {
 							<i className='fas fa-shopping-cart'></i>
 						</a>
 					</Link>
-					<i className='mt-2 text-gray-400 md:pl-6 fas fa-bars'></i>
+					<i
+						className='mt-2 text-gray-400 cursor-pointer md:pl-6 fas fa-user'
+						onClick={e => toggler(e)}
+					></i>
 				</div>
 			</div>
+			{/* Menu On Toggle */}
+			<div
+				className={classNames(
+					'self-end transition duration-200 border w-60 transform -translate-x-2 translate-y-14 z-50 bg-white absolute',
+					{
+						hidden: toggle,
+					}
+				)}
+			>
+				{/* Account Section */}
+				<div className='flex flex-row items-center px-4 py-2 space-x-4 hover:bg-gray-50'>
+					<Link href='/account'>
+						<img
+							src='https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'
+							className='w-8 h-8 rounded-full cursor-pointer'
+						/>
+					</Link>
+					<div>
+						<h2 className='text-sm font-semibold text-gray-600 capitalize'>
+							Stewie Griffin
+						</h2>
+						<p className='text-xs text-gray-400'>stewie@gmail.com</p>
+						<Link href='/account'>
+							<a className='text-xs font-semibold text-blue-400 hover:underline'>
+								Manage your account
+							</a>
+						</Link>
+					</div>
+				</div>
 
-			{/* Menu Items On Toggle */}
-			<div className='flex items-center'></div>
+				{/* Menu List */}
+				<div className='py-2 border border-l-0 border-r-0'>
+					<Link href='/account'>
+						<a className='flex flex-row items-center justify-start py-2 pl-4 space-x-4 text-sm text-gray-600 hover:bg-gray-100'>
+							<i className='text-gray-400 fas fa-user'></i>
+							<p className='text-xs font-medium'>Account</p>
+						</a>
+					</Link>
+					<Link href='/orders'>
+						<a className='flex flex-row items-center justify-start py-2 pl-4 space-x-4 text-sm text-gray-600 hover:bg-gray-100'>
+							<i className='text-gray-400 fas fa-shopping-bag'></i>
+							<p className='text-xs font-medium'>Purchases and Orders</p>
+						</a>
+					</Link>
+					<Link href='/'>
+						<a className='flex flex-row items-center justify-start py-2 pl-4 space-x-4 text-sm text-gray-600 hover:bg-gray-100'>
+							<i className='text-gray-400 fas fa-question'></i>
+							<p className='text-xs font-medium'>Help</p>
+						</a>
+					</Link>
+					<Link href='/'>
+						<a className='flex flex-row items-center justify-start py-2 pl-4 space-x-4 text-sm text-gray-600 hover:bg-gray-100'>
+							<i className='text-gray-400 fas fa-comments'></i>
+							<p className='text-xs font-medium'>Send Feedback</p>
+						</a>
+					</Link>
+				</div>
+
+				{/* Login / Logout */}
+				<button className='flex flex-row items-center justify-start w-full py-4 pl-4 space-x-4 text-sm text-gray-600 hover:bg-gray-100'>
+					<i className='text-gray-400 fas fa-sign-out-alt'></i>
+					<p className='text-xs font-medium'>Sign Out</p>
+				</button>
+			</div>
 		</nav>
 	);
 };
